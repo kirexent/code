70e356ddd1f79a82547ccc17df7b854a9fd3964c
Feshchenko Stanislav <stanislav.feschenko@bhnetwork.com>
2020-04-20 19:44:51 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'css/bundle.css',
-            chunkFilename: 'css/[id].css',
         }),
         new HtmlWebpackPlugin({
             filename: 'onpug.html',
